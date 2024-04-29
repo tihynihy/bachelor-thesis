b39384ab004a7523bd8dfae15718e880844c5a98
Nihad Jusović <76163793+tihynihy@users.noreply.github.com>
2024-04-29 23:40:12 +0200

    Update two-factor-authentication-form.blade.php

--- resources/views/profile/two-factor-authentication-form.blade.php
+++ resources/views/profile/two-factor-authentication-form.blade.php
@@ -1,9 +1,4 @@
-<x-action-section>
-    <x-slot name="title">
-        {{ __('Two Factor Authentication') }}
-    </x-slot>
-
-    <x-slot name="description">
+ <x-slot name="description">
         {{ __('Add additional security to your account using two factor authentication.') }}
     </x-slot>
 
